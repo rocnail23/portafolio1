086dd324591c9abcec76c5265f118a9cde352284
rocnail23 <jdanielrojas16@gmail.com>
2023-06-19 02:50:44 -0400

    last commit

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import yo from "../assets/yo3.0.webp"
+import {motion} from "framer-motion"
 const Me = ({setIsLoading}) => {
   const [animation, setAnimation] = useState(false)
 
   useEffect(() => {
     setAnimation(true)
   })
- 
+  
+
   return (
     <article className='me nunito'>
         <header className='photo'>
@@ -14,9 +16,15 @@ const Me = ({setIsLoading}) => {
         </header>
         <main className='me_main'>
         <h1 className='title'>Desarrollador</h1>
-        
-        <p className={animation ? `appear` : ""}>¡Bienvenido a mi rincón en la web! <br /> como desarrollador web, me encanta transformar conceptos en sitios web interactivos y atractivos <br /> <span className='keys'>¡</span>adelante y descubre mi trabajo<span className='keys'>!</span>
-        </p>
+        <button className='calendly' onClick={() =>  Calendly.initPopupWidget({
+      url: 'https://calendly.com/jdanielrojas16/30min?hide_event_type_details=1&hide_gdpr_banner=1&background_color=1a1a1a&text_color=ffffff&primary_color=ff0004'
+    })}>agenda una reunion conmigo</button>
+        <motion.p 
+          initial={{y:100, opacity: 0}}
+          animate={{y:0, opacity:1}}
+          transition={{duration: 1}}
+        >¡Bienvenido a mi rincón en la web! <br /> como desarrollador web, me encanta transformar conceptos en sitios web interactivos y atractivos <br /> <span className='keys'>¡</span>adelante y descubre mi trabajo<span className='keys'>!</span>
+        </motion.p>
 
          
         </main>
