889de80e206e40114bb9974587f44232232393c7
rocnail23 <jdanielrojas16@gmail.com>
2023-06-19 11:43:56 -0400

    l

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -11,13 +11,14 @@ const Technologies = () => {
         >Tecnologias <span><i class='bx bx-code-block bx-sm'></i></span></h3>
         <main>
         <ul className='technologies_list'>
-           <li><h4>Html</h4></li>  
-           <li><h4>Css</h4></li>  
-           <li><h4>JavaScript</h4></li>
-           <li><h4>React</h4></li> 
-           <li><h4>NodeJs</h4></li> 
-           <li><h4>Express</h4></li>
-           <li><h4>Git</h4></li>    
+           <li><h4>Html</h4><span className='icons'><i class='bx bxl-html5 bx-sm'></i></span></li>  
+           <li><h4>Css</h4><span className='icons'></span><i class='bx bxl-css3 bx-sm' ></i></li>  
+           <li><h4>JavaScript</h4><span><i class='bx bxl-javascript' ></i></span></li>
+           <li><h4>React</h4><span className='icons'><i class='bx bxl-react bx-sm' ></i></span></li> 
+           <li><h4>Redux</h4><span className='icons'><i class='bx bxl-redux bx-sm' ></i></span></li> 
+           <li><h4>NodeJs</h4><span className='icons'><i class='bx bxl-nodejs bx-sm' ></i></span></li> 
+           <li><h4>Express</h4><span className='nunito'>ex</span></li>
+           <li><h4>Github</h4><span className='icons'><i class='bx bxl-github bx-sm'></i></span></li>    
         </ul>
         </main>
     </motion.article
