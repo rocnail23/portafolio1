68026b47eaa23dcea581822dec667e481a08b5db
rocnail23 <jdanielrojas16@gmail.com>
2023-06-24 17:40:21 -0400

    l

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
  
   return (
     <Fragment>
-       <ParticleBackground/>
+       
       {isLoading ? <div className='negra'></div> : null }
      <Header/>
      <main className='container'>
